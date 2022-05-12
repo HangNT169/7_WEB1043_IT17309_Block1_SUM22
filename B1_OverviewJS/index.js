8e723b4f62efec299ee1d143df9e9d3127de75b3
HangNT169 <hN160499aA@>
2022-05-12 10:29:56 +0700

    B2_Review Toan tu

--- B1_OverviewJS/index.js
+++ B1_OverviewJS/index.js
@@ -1,3 +1,7 @@
 document.write("JS ngoai file HTML");
 console.log("test cosole log JS");
 alert("Thong bao alter");
+/**
+ * 1.Nhap ten, tuoi, dia chi tu ban phim va alter ra
+ * 2. Nhap 1 so tu ban phim va kiem tra xem no co chia het cho 10 khong
+ */
