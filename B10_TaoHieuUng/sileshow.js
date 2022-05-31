06a71d9d5010942ad0ea595226f7385bab273602
HangNT169 <hN160499aA@>
2022-05-31 11:24:20 +0700

    B10_SlideShow

---
+++ B10_TaoHieuUng/sileshow.js
@@ -0,0 +1,55 @@
+let imageObjects = [];
+let indexHienTai = 0;
+let intevarId;
+// Nap tat ca cac anh vao body khi project chay
+function loadImage() {
+  // B1: Tao ra 1 mang chua ten tat ca cac anh trong thu muc
+  const images = ["cat.jpg", "chicken.jpg", "dog.jpg", "monkey.jpg", "pig.jpg"];
+
+  // B2: Tao 1 ra 1 bien la ten cua folder chua anh
+  const imgFolderName = "images/";
+
+  //B3:Push anh vao html
+  images.forEach(function (e) {
+    // Khoi tao 1 doi tuong image
+    let img = new Image();
+    // gan duong dan anh
+    img.src = imgFolderName + e;
+    // push anh
+    imageObjects.push(img);
+  });
+
+  // B4: Xet lai vao html
+  document.img_pet.src = imageObjects[indexHienTai].src;
+}
+
+function previousImage() {
+  indexHienTai--;
+  if (indexHienTai < 0) {
+    indexHienTai = imageObjects.length - 1;
+  }
+  //  Xet lai vao html
+  document.img_pet.src = imageObjects[indexHienTai].src;
+}
+
+function start() {
+  if (intevarId === undefined) {
+    // set time cho no
+    intevarId = setInterval(function () {
+      nextImage();
+    }, 1500);
+  }
+}
+
+function stop() {
+  clearInterval(intevarId);
+  intevarId = undefined;
+}
+function nextImage() {
+  indexHienTai++;
+  if (indexHienTai >= imageObjects.length) {
+    indexHienTai = 0;
+  }
+  //  Xet lai vao html
+  document.img_pet.src = imageObjects[indexHienTai].src;
+}
